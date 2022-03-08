a7dad17da816724fa4bf52f8319ec989f96f6b1c
Simone Saputo <simonesaputo0@gmail.com>
2022-03-08 17:40:14 +0100

    generator

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,5 +6,20 @@ let colorePreferito = prompt("Please enter your favourite color ?");
 
 let etàUtente = prompt("Please enter your age ?");
 
-document.getElementById("password_generator").innerHTML = nomeUtente + cognomeUtente + colorePreferito + etàUtente;
+document.getElementById("password_generator").innerHTML = "Your password is: " + nomeUtente + cognomeUtente + colorePreferito + etàUtente;
 
+
+
+function password() {
+    let nomeUtente_2 = prompt("Hello, please enter your name ?");
+    
+    let cognomeUtente_2 = prompt("Please enter your surname?");
+    
+    let colorePreferito_2 = prompt("Please enter your favourite color ?");
+    
+    let etàUtente_2 = prompt("Please enter your age ?");
+    
+    if (nomeUtente_2 && cognomeUtente && colorePreferito_2 && etàUtente_2 != null){
+    document.getElementById("password_generator_2").innerHTML = "Your password is: " + nomeUtente_2 + cognomeUtente_2 + colorePreferito_2 + etàUtente_2 + " ... How are ready for scam?";
+    }
+}
