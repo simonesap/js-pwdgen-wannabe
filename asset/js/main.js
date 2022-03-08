e5a8e475b11999738c4c7901721fb7da896a05b5
Simone Saputo <simonesaputo0@gmail.com>
2022-03-08 16:46:11 +0100

    main-js

---
+++ asset/js/main.js
@@ -0,0 +1,10 @@
+let nomeUtente = prompt("Hello, please enter your name ?");
+
+let cognomeUtente = prompt("Please enter your surname?");
+
+let colorePreferito = prompt("Please enter your favourite color ?");
+
+let etàUtente = prompt("Please enter your age ?");
+
+document.getElementById("password_generator").innerHTML = nomeUtente + cognomeUtente + colorePreferito + etàUtente;
+
